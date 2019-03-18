cb2b70f16b47ef5d4b67aa80371a3b8f5918a5d0
kinchad <kin0307003070@gmail.com>
2019-03-18 16:01:12 +0800

    Add location API

--- app/src/main/java/com/example/kin/assignment/homePage.java
+++ app/src/main/java/com/example/kin/assignment/homePage.java
@@ -1,5 +1,6 @@
 package com.example.kin.assignment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,6 +49,7 @@ public class homePage extends AppCompatActivity
         lvCurrency = (ListView)findViewById(R.id.lvCurrency);
         currencyRates = getResources().getStringArray(R.array.currencyRates);
         ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,currencyRates);
+        lvCurrency.setAdapter(adapter);
     }
 
     @Override
@@ -88,12 +90,15 @@ public class homePage extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
+        if (id == R.id.homepage) {
+            Intent intent = new Intent(this,homePage.class);
+            startActivity(intent);
+        } else if (id == R.id.recommend) {
+            Intent intent = new Intent(this,recommendCur.class);
+            startActivity(intent);
+        } else if (id == R.id.converter) {
+            Intent intent = new Intent(this,converter.class);
+            startActivity(intent);
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
